554133fb076b2cabf4deb0b012cb4952f4d06e80
sundarajuaswintdr@gmail.com <sundarajuaswintdr@gmail.com>
2025-09-22 21:17:26 +0530

    Initial project commit

--- CourseRegistrationSystem Ui/myScript.js
+++ CourseRegistrationSystem Ui/myScript.js
@@ -1,39 +1,42 @@
-function availCourses(){
-    fetch("http://localhost:8080/courses").
-    then((response) => response.json()).
-    then((courses) => {
-        var dataTable = document.getElementById("courseTable")
-        
-        courses.forEach(course => {
-
-            var row = `<tr>
-            <td>${course.courseId}</td>
-            <td>${course.courseName}</td>
-            <td>${course.trainer}</td>
-            <td>${course.durationInWeeks}</td>
-            </tr>`
-            
-            dataTable.innerHTML+=row;
-        });
-    })
-}
-
-
-function regStudents() {
-    fetch("http://localhost:8080/courses/enrolled").
-        then((response) => response.json()).
-        then((students) => {
-            var dataTable = document.getElementById("studentsTable")
-
-            students.forEach(student => {
-
-            var row = `<tr>
-            <td>${student.name}</td>
-            <td>${student.emailId}</td>
-            <td>${student.courseName}</td>
-            </tr>`
-
+function availCourses() {
+    fetch("http://localhost:8080/courses")
+        .then((response) => response.json())
+        .then((courses) => {
+            var dataTable = document.getElementById("courseTable");
+            courses.forEach(course => {
+                var row = `<tr>
+          <td class="px-6 py-3">${course.courseId}</td>
+          <td class="px-6 py-3">${course.courseName}</td>
+          <td class="px-6 py-3">${course.trainer}</td>
+          <td class="px-6 py-3">${course.durationInWeeks}</td>
+        </tr>`;
                 dataTable.innerHTML += row;
             });
-        })
+        });
 }
+
+function regStudents() {
+    fetch("http://localhost:8080/courses/enrolled")
+        .then((response) => response.json())
+        .then((students) => {
+            var dataTable = document.getElementById("studentsTable");
+            students.forEach(student => {
+                var row = `<tr>
+          <td class="px-6 py-3">${student.name}</td>
+          <td class="px-6 py-3">${student.emailId}</td>
+          <td class="px-6 py-3">${student.courseName}</td>
+        </tr>`;
+                dataTable.innerHTML += row;
+            });
+        });
+}
+
+
+window.onload = function () {
+    if (document.getElementById("courseTable")) {
+        availCourses();
+    }
+    if (document.getElementById("studentsTable")) {
+        regStudents();
+    }
+};
